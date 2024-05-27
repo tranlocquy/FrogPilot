ebc2887b4b72af6c3915aee521d0e7fa7474fa1f
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-26 23:15:27 -0700

    Controls - Map Turn Speed Control
    Slow down for anticipated curves detected by the downloaded maps.
    
    Credit goes to Pfeiferj!
    
    https: //github.com/pfeiferj
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

--- selfdrive/frogpilot/controls/frogpilot_planner.py
+++ selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -15,6 +15,7 @@ from openpilot.selfdrive.controls.lib.longitudinal_planner import A_CRUISE_MIN, 
 from openpilot.selfdrive.frogpilot.controls.lib.conditional_experimental_mode import ConditionalExperimentalMode
 from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_functions import calculate_lane_width, calculate_road_curvature
 from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_variables import CITY_SPEED_LIMIT, CRUISING_SPEED
+from openpilot.selfdrive.frogpilot.controls.lib.map_turn_speed_controller import MapTurnSpeedController
 
 A_CRUISE_MIN_ECO = A_CRUISE_MIN / 5
 A_CRUISE_MIN_SPORT = A_CRUISE_MIN / 2
@@ -36,12 +37,14 @@ class FrogPilotPlanner:
     self.params_memory = Params("/dev/shm/params")
 
     self.cem = ConditionalExperimentalMode()
+    self.mtsc = MapTurnSpeedController()
 
     self.slower_lead = False
     self.tracking_lead = False
 
     self.acceleration_jerk = 0
     self.danger_jerk = 0
+    self.mtsc_target = 0
     self.road_curvature = 0
     self.speed_jerk = 0
 
@@ -50,6 +53,7 @@ class FrogPilotPlanner:
 
     v_cruise_kph = min(controlsState.vCruise, V_CRUISE_UNSET)
     v_cruise = v_cruise_kph * CV.KPH_TO_MS
+    v_cruise_changed = self.mtsc_target < v_cruise
 
     v_ego = max(carState.vEgo, 0)
     v_lead = self.lead_one.vLead
@@ -71,6 +75,8 @@ class FrogPilotPlanner:
 
     if controlsState.experimentalMode:
       self.min_accel = ACCEL_MIN
+    elif v_cruise_changed:
+      self.min_accel = A_CRUISE_MIN
     elif frogpilot_toggles.deceleration_profile == 1:
       self.min_accel = A_CRUISE_MIN_ECO
     elif frogpilot_toggles.deceleration_profile == 2:
@@ -149,7 +155,19 @@ class FrogPilotPlanner:
     v_ego_cluster = max(carState.vEgoCluster, v_ego)
     v_ego_diff = v_ego_cluster - v_ego
 
-    targets = []
+    # Pfeiferj's Map Turn Speed Controller
+    if frogpilot_toggles.map_turn_speed_controller and v_ego > CRUISING_SPEED and controlsState.enabled:
+      mtsc_active = self.mtsc_target < v_cruise
+      self.mtsc_target = np.clip(self.mtsc.target_speed(v_ego, carState.aEgo), CRUISING_SPEED, v_cruise)
+
+      if frogpilot_toggles.mtsc_curvature_check and self.road_curvature < 1.0 and not mtsc_active:
+        self.mtsc_target = v_cruise
+      if self.mtsc_target == CRUISING_SPEED:
+        self.mtsc_target = v_cruise
+    else:
+      self.mtsc_target = v_cruise if v_cruise != V_CRUISE_UNSET else 0
+
+    targets = [self.mtsc_target]
     filtered_targets = [target if target > CRUISING_SPEED else v_cruise for target in targets]
 
     return min(filtered_targets)
@@ -165,6 +183,8 @@ class FrogPilotPlanner:
     frogpilotPlan.speedJerk = J_EGO_COST * float(self.speed_jerk)
     frogpilotPlan.speedJerkStock = J_EGO_COST * float(self.base_speed_jerk)
     frogpilotPlan.tFollow = float(self.t_follow)
+
+    frogpilotPlan.adjustedCruise = float(self.mtsc_target * (CV.MS_TO_KPH if frogpilot_toggles.is_metric else CV.MS_TO_MPH))
 
     frogpilotPlan.conditionalExperimentalActive = self.cem.experimental_mode
 
