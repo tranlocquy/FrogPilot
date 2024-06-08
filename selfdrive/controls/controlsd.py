2d557043d42d6b4c0a68068d08e1f3a50be89d60
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-08 08:01:09 -0700

    Visuals - Custom Themes - Holiday Themes
    The openpilot theme changes according to the current/upcoming holiday. Minor holidays last a day, while major holidays (Easter, Christmas, Halloween, etc.) last a week.

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -194,6 +194,7 @@ class Controls:
     self.always_on_lateral_active = False
     self.always_on_lateral_disabled = False
     self.drive_added = False
+    self.holiday_theme_alerted = False
     self.onroad_distance_pressed = False
     self.openpilot_crashed_triggered = False
     self.previous_traffic_mode = False
@@ -919,6 +920,10 @@ class Controls:
 
       if green_light:
         self.events.add(EventName.greenLight)
+
+    if not self.holiday_theme_alerted and self.frogpilot_toggles.current_holiday_theme != 0 and self.sm.frame * DT_CTRL >= 10:
+      self.events.add(EventName.holidayActive)
+      self.holiday_theme_alerted = True
 
     if self.sm['frogpilotPlan'].leadDeparting:
       self.events.add(EventName.leadDeparting)
