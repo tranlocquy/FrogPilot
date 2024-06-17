96a35fb2c715ee6ffd5b41d77351b283bd9dfe9b
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-17 09:56:15 -0700

    FrogPilot setup - FrogPilot Planner

---
+++ selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+import cereal.messaging as messaging
+
+from openpilot.common.conversions import Conversions as CV
+from openpilot.common.numpy_fast import interp
+from openpilot.common.params import Params
+
+from openpilot.selfdrive.car.interfaces import ACCEL_MIN, ACCEL_MAX
+from openpilot.selfdrive.controls.lib.drive_helpers import V_CRUISE_UNSET
+from openpilot.selfdrive.controls.lib.longitudinal_mpc_lib.long_mpc import A_CHANGE_COST, COMFORT_BRAKE, DANGER_ZONE_COST, J_EGO_COST, STOP_DISTANCE, \
+                                                                           get_jerk_factor, get_safe_obstacle_distance, get_stopped_equivalence_factor, get_T_FOLLOW
+from openpilot.selfdrive.controls.lib.longitudinal_planner import A_CRUISE_MIN, get_max_accel
+
+from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_variables import CITY_SPEED_LIMIT, CRUISING_SPEED
+
+class FrogPilotPlanner:
+  def __init__(self):
+    self.params_memory = Params("/dev/shm/params")
+
+    self.tracking_lead = False
+
+    self.acceleration_jerk = 0
+    self.danger_jerk = 0
+    self.speed_jerk = 0
+
+  def update(self, carState, controlsState, frogpilotCarControl, frogpilotCarState, frogpilotNavigation, modelData, radarState):
+    self.lead_one = radarState.leadOne
+
+    v_cruise_kph = min(controlsState.vCruise, V_CRUISE_UNSET)
+    v_cruise = v_cruise_kph * CV.KPH_TO_MS
+
+    v_ego = max(carState.vEgo, 0)
+    v_lead = self.lead_one.vLead
+
+    lead_distance = self.lead_one.dRel
+    stopping_distance = STOP_DISTANCE
+
+    if controlsState.experimentalMode:
+      self.max_accel = ACCEL_MAX
+    else:
+      self.max_accel = get_max_accel(v_ego)
+
+    if controlsState.experimentalMode:
+      self.min_accel = ACCEL_MIN
+    else:
+      self.min_accel = A_CRUISE_MIN
+
+    self.base_acceleration_jerk, self.base_speed_jerk = get_jerk_factor(controlsState.personality)
+    self.base_danger_jerk = 1.0
+    self.t_follow = get_T_FOLLOW(controlsState.personality)
+
+    if self.tracking_lead:
+      self.update_follow_values(lead_distance, stopping_distance, v_ego, v_lead)
+    else:
+      self.acceleration_jerk = self.base_acceleration_jerk
+      self.danger_jerk = self.base_danger_jerk
+      self.speed_jerk = self.base_speed_jerk
+
+    self.v_cruise = self.update_v_cruise(carState, controlsState, frogpilotCarState, frogpilotNavigation, modelData, v_cruise, v_ego)
+
+    if v_ego > CRUISING_SPEED:
+      self.tracking_lead = self.lead_one.status
+
+  def update_follow_values(self, lead_distance, stopping_distance, v_ego, v_lead):
+
+  def update_v_cruise(self, carState, controlsState, frogpilotCarState, frogpilotNavigation, modelData, v_cruise, v_ego):
+    v_cruise_cluster = max(controlsState.vCruiseCluster, v_cruise) * CV.KPH_TO_MS
+    v_cruise_diff = v_cruise_cluster - v_cruise
+
+    v_ego_cluster = max(carState.vEgoCluster, v_ego)
+    v_ego_diff = v_ego_cluster - v_ego
+
+    targets = []
+    filtered_targets = [target if target > CRUISING_SPEED else v_cruise for target in targets]
+
+    return min(filtered_targets)
+
+  def publish(self, sm, pm):
+    frogpilot_plan_send = messaging.new_message('frogpilotPlan')
+    frogpilot_plan_send.valid = sm.all_checks(service_list=['carState', 'controlsState'])
+    frogpilotPlan = frogpilot_plan_send.frogpilotPlan
+
+    frogpilotPlan.accelerationJerk = A_CHANGE_COST * float(self.acceleration_jerk)
+    frogpilotPlan.accelerationJerkStock = A_CHANGE_COST * float(self.base_acceleration_jerk)
+    frogpilotPlan.dangerJerk = DANGER_ZONE_COST * float(self.danger_jerk)
+    frogpilotPlan.speedJerk = J_EGO_COST * float(self.speed_jerk)
+    frogpilotPlan.speedJerkStock = J_EGO_COST * float(self.base_speed_jerk)
+    frogpilotPlan.tFollow = float(self.t_follow)
+
+    frogpilotPlan.maxAcceleration = self.max_accel
+    frogpilotPlan.minAcceleration = self.min_accel
+
+    frogpilotPlan.vCruise = float(self.v_cruise)
+
+    pm.send('frogpilotPlan', frogpilot_plan_send)
